df8223a53a60dae5192c974cf969c45f94391545
MelisaKihn <144850570+MelisaKihn@users.noreply.github.com>
2023-11-06 17:37:26 -0300

    Add files via upload

---
+++ js/javas.js
@@ -0,0 +1,11 @@
+function fondooscuro () {document.body.style.backgroundColor= "black"; 
+document.getElementById ("black").style.backgroundColor= "black";
+document.getElementById ("black").style.color= "white";
+}
+
+function fondoclaro () {document.body.style.backgroundColor= "white";
+document.getElementById ("black").style.backgroundColor= "#FEDEA5"
+document.getElementById ("black").style.color= "black";
+}
+
+function pop() {alert("Desarrolladora: Melisa Kihn, email: melisakihn@gmail.com")}
